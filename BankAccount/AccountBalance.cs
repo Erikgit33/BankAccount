4451ab7bca91f7797579a063ff194a5ca1a9098f
Erik Bäckström <Erik Bckstrm@DESKTOP-MEVT6FK>
2024-09-11 09:52:31 +0200

    Add project files.

---
+++ BankAccount/AccountBalance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class BankAccount
+    {
+        int balance;
+
+        public void Deposit(int d)
+        {
+            balance += d;
+        }
+
+        public void Withdraw(int w)
+        {   
+            if (w <= balance)
+            {
+                balance -= w;
+            }
+        }
+        
+        public int GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount();
+            BankAccount account2 = new BankAccount();
+
+            account1.Deposit(100);
+            account2.Deposit(40);
+            Console.WriteLine();
+
+            Console.WriteLine("Account1 deposits 100");
+            Console.WriteLine("Account2 deposits 40");
+            Console.WriteLine();
+
+            Console.WriteLine("Account 1: " + account1.GetBalance());
+            Console.WriteLine("Account 2: " + account2.GetBalance());
+
+            account1.Withdraw(80);
+            account2.Deposit(80);
+            Console.WriteLine();
+
+            Console.WriteLine("Account1 withdraws 80");
+            Console.WriteLine("Account2 deposts 80");
+            Console.WriteLine();
+
+            Console.WriteLine("Account 1: " + account1.GetBalance());
+            Console.WriteLine("Account 2: " + account2.GetBalance());
+
+            account1.Withdraw(2500);
+            account2.Withdraw(50);
+            Console.WriteLine();
+
+            Console.WriteLine("Account1 withdraws 2500");
+            Console.WriteLine("Account2 withdraws 50");
+            Console.WriteLine();
+
+            Console.WriteLine("Account 1: " + account1.GetBalance());
+            Console.WriteLine("Account 2: " + account2.GetBalance());
+        }
+    }
+}
